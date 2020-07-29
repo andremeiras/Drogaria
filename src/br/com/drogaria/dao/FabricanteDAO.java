b21df5535561810ae60fcf615728bf873fb5e2db
André Meira <andremeirati@gmail.com>
2020-07-29 15:04:03 -0300

    Criado nova branch para testar o método de editar() com JUnit

--- src/br/com/drogaria/dao/FabricanteDAO.java
+++ src/br/com/drogaria/dao/FabricanteDAO.java
@@ -48,20 +48,81 @@ public class FabricanteDAO {
 
 	public Fabricante buscarPorCodigo(Long codigo) {
 		Session sessao = HibernateUtil.getSessionFactory().openSession();
-		
+
 		Fabricante fabricante = null;
-		
+
 		try {
 			Query consulta = sessao.getNamedQuery("Fabricante.buscarPorCodigo");
 			consulta.setLong("codigo", codigo);
-			
+
 			fabricante = (Fabricante) consulta.uniqueResult();
-			
+
 		} catch (RuntimeException ex) {
 			throw ex;
 		} finally {
 			sessao.close();
 		}
 		return fabricante;
+	}
+
+	public void excluir(Fabricante fabricante) {
+		Session sessao = HibernateUtil.getSessionFactory().openSession();
+
+		Transaction transacao = null;
+
+		try {
+			transacao = sessao.beginTransaction();
+			sessao.delete(fabricante);
+			transacao.commit();
+
+		} catch (RuntimeException ex) {
+			if (transacao != null) {
+				transacao.rollback();
+			}
+		} finally {
+			sessao.close();
+		}
+	}
+
+	public void excluir(Long codigo) {
+		Session sessao = HibernateUtil.getSessionFactory().openSession();
+
+		Transaction transacao = null;
+
+		try {
+			transacao = sessao.beginTransaction();
+			Fabricante fabricante = buscarPorCodigo(codigo);
+			sessao.delete(fabricante);
+			transacao.commit();
+
+		} catch (RuntimeException ex) {
+			if (transacao != null) {
+				transacao.rollback();
+			}
+		} finally {
+			sessao.close();
+		}
+	}
+
+	public void editar(Fabricante fabricante) {
+		Session sessao = HibernateUtil.getSessionFactory().openSession();
+		Transaction transacao = null;
+
+		try {
+			transacao = sessao.beginTransaction();
+			
+			Fabricante fabricanteEditar = buscarPorCodigo(fabricante.getCodigo());
+			fabricanteEditar.setDescricao(fabricante.getDescricao());
+						
+			sessao.update(fabricanteEditar);
+			transacao.commit();
+
+		} catch (RuntimeException ex) {
+			if (transacao != null) {
+				transacao.rollback();
+			}
+		} finally {
+			sessao.close();
+		}
 	}
 }
--- src/br/com/drogaria/test/FabricanteDAOTest.java
+++ src/br/com/drogaria/test/FabricanteDAOTest.java
@@ -10,23 +10,25 @@ import br.com.drogaria.dao.FabricanteDAO;
 import br.com.drogaria.domain.Fabricante;
 
 public class FabricanteDAOTest {
+
+	private static Scanner sc;
+	private Long codigo;
+	private String descricao;
+
 	@Test
 	@Ignore
 	public void salvar() {
+		sc = new Scanner(System.in);
 
-		Fabricante f1 = new Fabricante();
-		f1.setDescricao("DESCRICAO 1");
+		System.out.print("Digite um fabricante: ");
+		String descricao = sc.nextLine();
 
-		Fabricante f2 = new Fabricante();
-		f2.setDescricao("DESCRICAO 2");
-
-		Fabricante f3 = new Fabricante();
+		Fabricante fabricante = new Fabricante();
+		fabricante.setDescricao(descricao);
 
 		FabricanteDAO dao = new FabricanteDAO();
 
-		dao.salvar(f1);
-		dao.salvar(f2);
-		dao.salvar(f3);
+		dao.salvar(fabricante);
 	}
 
 	@Test
@@ -37,19 +39,86 @@ public class FabricanteDAOTest {
 
 		System.out.println(fabricantes);
 	}
-	
+
 	@Test
 	@Ignore
 	public void buscarPorCodigo() {
-		Scanner sc = new Scanner(System.in);
 		System.out.print("Digite um código: ");
-		Long codigo = sc.nextLong();
-		
+		codigo = sc.nextLong();
+
 		FabricanteDAO dao = new FabricanteDAO();
 		Fabricante fabricante = dao.buscarPorCodigo(codigo);
-		
+
 		System.out.println(fabricante);
-	
+	}
+
+	// Resolvendo com lógica (if else)
+	@Test
+	@Ignore
+	public void excluir() {
+		System.out.println("Digite um código: ");
+		codigo = sc.nextLong();
+
+		FabricanteDAO dao = new FabricanteDAO();
+		Fabricante fabricante = dao.buscarPorCodigo(codigo);
+
+		if (fabricante != null) {
+			dao.excluir(fabricante);
+		} else {
+			System.out.println("Fabricante " + codigo + " não existe");
+		}
+
+		// esta forma existe quando não for trabalhar com persistência
+//		Fabricante fabricante = new Fabricante();
+//		fabricante.setCodigo(codigo);
+//
+//		FabricanteDAO dao = new FabricanteDAO();
+//		dao.excluir(fabricante);
+
+	}
+
+	// Resolvendo com Transação
+	@Test
+	@Ignore
+	public void excluirPorCodigo() {
+		System.out.print("Digite um código: ");
+		codigo = sc.nextLong();
+
+		FabricanteDAO dao = new FabricanteDAO();
+
+		dao.excluir(codigo);
+	}
+
+	@Test
+	public void editar() {
+
+		sc = new Scanner(System.in);
+
+		Fabricante fabricante = new Fabricante();
+		FabricanteDAO dao = new FabricanteDAO();
+
+		System.out.print("Digite um código: ");
+		codigo = sc.nextLong();
+		
+		dao.buscarPorCodigo(fabricante.getCodigo()).setCodigo(codigo);
+
+		if (fabricante.getCodigo() == null) {
+			System.out.println("nem chegou aqui");
+			System.out.println(fabricante.getCodigo());
+			System.out.println("Fabricante selecionado: " + dao.buscarPorCodigo(fabricante.getCodigo()).getDescricao());
+			
+			System.out.println("\n");
+
+			System.out.print("Digite a nova descrição: ");
+			descricao = sc.nextLine();
+			fabricante.setDescricao(descricao);
+
+			dao.editar(fabricante);
+		} else 
+		{
+			System.out.println("ERRO!!!!");
+		}
+		sc.close();
 	}
 
 }
